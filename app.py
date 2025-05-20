8aa6714c5c5b2348ec1644989e95dceedb42b42f
vinay g <vinayghbk@gmail.com>
2025-05-20 12:17:53 +0530

    Bind Flask to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import requests
 
@@ -55,5 +56,6 @@ def get_places_data(food_name, location):
             })
     return places
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
